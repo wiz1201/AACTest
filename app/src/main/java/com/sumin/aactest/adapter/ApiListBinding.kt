10e3e264464d6815d1f4e0485d3f58fb0b7b8d13
su-min <android0413@gmail.com>
2020-08-08 02:06:34 +0900

    Data Binding 추가
    1. Data Binding 추가

---
+++ app/src/main/java/com/sumin/aactest/adapter/ApiListBinding.kt
@@ -0,0 +1,49 @@
+package com.sumin.aactest.adapter
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.sumin.aactest.APIFragmentAdapter
+import com.sumin.aactest.LocalFragmentAdapter
+import com.sumin.aactest.R
+import com.sumin.aactest.data.User
+import com.sumin.aactest.data.UserItems
+
+@BindingAdapter("app:items")
+fun setItems(recyclerView: RecyclerView, items: List<UserItems>?) {
+    (recyclerView.adapter as APIFragmentAdapter).setUsers(items)
+}
+
+@BindingAdapter("app:users")
+fun setUsers(recyclerView: RecyclerView, items: List<User>?) {
+    (recyclerView.adapter as LocalFragmentAdapter).setUsers(items)
+}
+
+
+@BindingAdapter("app:likeImg")
+fun setLikeImage(view: ImageView, isLike: Boolean = false) {
+    /*if (isLike) view.setImageResource(R.mipmap.like_on_foreground)
+    else view.setImageResource(R.mipmap.like_off_foreground)*/
+}
+
+@BindingAdapter("app:profileImg")
+fun setProfileImg(view: ImageView?, url: String?) {
+    val umgUrl = url ?: ""
+
+    view?.let {
+        Glide.with(it.context)
+            .load(umgUrl)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .skipMemoryCache(false)
+            .placeholder(R.drawable.ic_launcher_foreground)
+            .into(it)
+    }
+}
+/*
+
+@BindingAdapter("app:itemDeco")
+fun setDecoration(view: RecyclerView, space: Float){
+    view.addItemDecoration(SpaceItem(space.toInt(), 3))
+}*/
