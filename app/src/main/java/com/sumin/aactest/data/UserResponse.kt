fb1147061d7fd269c412930256e8980b841076c6
su-min <android0413@gmail.com>
2020-08-08 02:09:35 +0900

    Git Response 추가
    1. Git Response 추가

---
+++ app/src/main/java/com/sumin/aactest/data/UserResponse.kt
@@ -0,0 +1,16 @@
+package com.sumin.aactest.data
+
+data class UserResponse(
+    val total_count: Int,
+    val incomplete_results: Boolean,
+    val items: List<UserItems>
+)
+
+data class UserItems(
+    val login: String, val id: Long, val node_id: String,
+    val avatar_url: String, val gravatar_id: String, val url: String,
+    val html_url: String, val followers_url: String, val following_url: String,
+    val gists_url: String, val starred_url: String, val subscriptions_url: String,
+    val organizations_url: String, val repos_url: String, val events_url: String,
+    val received_events_url: String, val type: String, val site_admin: Boolean,
+    val score: Double, val isLike:Boolean = false)
